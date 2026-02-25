dfdcc2fa740f5d64eccb83871da3b5181756c998
poonam <poonamr@finishline.com>
2026-02-25 21:44:45 +0530

    tpoic 4 operator

--- src/Operators.java
+++ src/Operators.java
@@ -1,5 +1,42 @@
 public class Operators {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+    int a=100;
+    int b=50;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+
+        System.out.println(a++);
+        System.out.println(a++);
+        System.out.println(a++);
+
+        System.out.println(b); //b value is 50
+        System.out.println(b--); //b value is still 50 here because before decrementing
+        System.out.println(b); // b value is now 49 since it decrement after printing 50 then decremented
+        System.out.println(--b);
+        System.out.println(--b);
+        System.out.println(--b);
+        int c=10;
+        c+=3;
+        System.out.println(c);
+        int sum=a+b;
+        System.out.println(sum);
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+        System.out.println(a==b);
+        System.out.println("==========");
+        System.out.println(3>2 &&10<9);
+        System.out.println(3>2 || 10<9);
+
+
+
+
 
 
 
